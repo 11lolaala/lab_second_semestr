656567235cc9d72bab724a73c4036ccd15ffd7a0
11lolaala <mishagrigoriev1710@gmail.com>
2026-02-20 11:28:02 +0200

    Update functions.hpp

--- Lab_03_test_copy/mylib_shared/inc/functions.hpp
+++ Lab_03_test_copy/mylib_shared/inc/functions.hpp
@@ -10,18 +10,20 @@ extern "C" {
 
 __declspec(dllexport) std::string current_time();
 
+static int loglevel_ = 1;
+
 class __declspec(dllexport) Logger {
 
     public:
-        __declspec(dllexport) void set_loglevel (int loglevel_);
-        __declspec(dllexport) void TRACER(int loglevel ,const std::string text);
-        __declspec(dllexport) void INFO(int loglevel, const std::string text,const std::string object_name);
-        __declspec(dllexport) void DEBUG(int loglevel, const std::string text, std::string object_name, int changes);
-        __declspec(dllexport) void WARNING(int loglevel, const std::string text, std::string object, std::string name_object,int line);
-        __declspec(dllexport) void ERROR(int loglevel, const std::string text, std::string object, std::string name_object, int line);
-        __declspec(dllexport) void CRITICAL(int loglevel, const std::string text, int object, std::string name_object, int line);
-
+        static __declspec(dllexport) void set_loglevel (int loglevel_);
+        static __declspec(dllexport) void TRACER(int loglevel ,const std::string text);
+        static __declspec(dllexport) void INFO(int loglevel, const std::string text,const std::string object_name);
+        static __declspec(dllexport) void DEBUG(int loglevel, const std::string text, std::string object_name, int changes);
+        static __declspec(dllexport) void WARNING(int loglevel, const std::string text, std::string object, std::string name_object,int line);
+        static __declspec(dllexport) void ERROR(int loglevel, const std::string text, std::string object, std::string name_object, int line);
+        static __declspec(dllexport) void CRITICAL(int loglevel, const std::string text, int object, std::string name_object, int line);
 
     private:
-        int loglevel_ = 1;
+        
 };
+
